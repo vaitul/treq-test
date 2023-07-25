e878adda3218cfc36e8703e2803ea9b9cc1ce466
Vaitul Bhayani <vaitulb@gmail.com>
2023-07-25 20:25:59 +0530

    fix: refactored input component

---
+++ src/components/input/index.jsx
@@ -0,0 +1,21 @@
+import "./input.css";
+
+const Input = (props) => {
+  const input = (
+    <input
+      className={`inputBox w-full p-5 ${props.label ? "mt-3" : ""}`}
+      {...props}
+    />
+  );
+
+  if (!props?.label) return input;
+
+  return (
+    <label className="inputLabel w-full flex flex-col">
+      {props?.label}
+      {input}
+    </label>
+  );
+};
+
+export default Input;
